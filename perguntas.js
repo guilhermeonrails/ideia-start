78b8228e98e2dfa23e49b45dea79d3ed1805d51b
guilhermelima <guilhermelimaruby@gmail.com>
2023-08-21 13:59:29 -0300

    ideia 1

--- perguntas.js
+++ perguntas.js
@@ -16,3 +16,56 @@ criarCard(
     "JavaScript, uma linguagem de programação amplamente usada para desenvolvimento web"
 );
 
+criarCard(
+    "Geografia",
+    "Qual é a capital da França?",
+    "A capital da França é Paris."
+);
+
+criarCard(
+    "Literatura",
+    "Quem escreveu a peça de teatro 'Romeu e Julieta'?",
+    "A peça 'Romeu e Julieta' foi escrita por William Shakespeare."
+);
+
+criarCard(
+    "Programação",
+    "O que é JavaScript'?",
+    "JavaScript, uma linguagem de programação amplamente usada para desenvolvimento web"
+);
+
+criarCard(
+    "Geografia",
+    "Qual é a capital da França?",
+    "A capital da França é Paris."
+);
+
+criarCard(
+    "Literatura",
+    "Quem escreveu a peça de teatro 'Romeu e Julieta'?",
+    "A peça 'Romeu e Julieta' foi escrita por William Shakespeare."
+);
+
+criarCard(
+    "Programação",
+    "O que é JavaScript'?",
+    "JavaScript, uma linguagem de programação amplamente usada para desenvolvimento web"
+);
+
+criarCard(
+    "Geografia",
+    "Qual é a capital da França?",
+    "A capital da França é Paris."
+);
+
+criarCard(
+    "Literatura",
+    "Quem escreveu a peça de teatro 'Romeu e Julieta'?",
+    "A peça 'Romeu e Julieta' foi escrita por William Shakespeare."
+);
+
+criarCard(
+    "Programação",
+    "O que é JavaScript'?",
+    "JavaScript, uma linguagem de programação amplamente usada para desenvolvimento web"
+);
