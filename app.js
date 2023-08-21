e1542f4ae2b11df6f3df7614b3392ad4715b2afc
guilhermelima <guilhermelimaruby@gmail.com>
2023-08-21 17:22:34 -0300

    blue

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-function criarCard(titulo, pergunta, resposta) {
+function criarCard(pergunta, resposta) {
   const cardContainer = document.getElementById("cardContainer");
 
   const card = document.createElement("card");
   card.className = "card";
   card.innerHTML = `
-    <h2>${titulo}</h2>
-    <p>${pergunta}</p>
+   
+    <h3>${pergunta}</h3>
     <p class="resposta">${resposta}</p>
   `;
 
-  card.setAttribute("role", "article");
-  card.setAttribute("aria-label", titulo);
 
   card.addEventListener("click", () => {
     const answer = card.querySelector(".resposta");
