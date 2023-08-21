4117f89a55567f41447baa1ce956cc21e14efe07
guilhermelima <guilhermelimaruby@gmail.com>
2023-08-21 19:54:52 -0300

    without title

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-function criarCard(titulo, pergunta, resposta) {
+function criarCard(pergunta, resposta) {
   const cardContainer = document.getElementById("cardContainer");
 
   const card = document.createElement("card");
   card.className = "card";
   card.innerHTML = `
-    <h2>${titulo}</h2>
     <p>${pergunta}</p>
     <p class="resposta">${resposta}</p>
   `;
-
-  card.setAttribute("role", "article");
-  card.setAttribute("aria-label", titulo);
 
   card.addEventListener("click", () => {
     const answer = card.querySelector(".resposta");
--- perguntas.js
+++ perguntas.js
@@ -1,128 +1,125 @@
 criarCard(
-    "Programação",
+
     "O que é um algoritmo?",
     "Um algoritmo é uma sequência de instruções que descrevem um processo para resolver um problema ou executar uma tarefa."
 );
 
 criarCard(
-    "Programação",
+
     "Qual é a diferença entre uma linguagem de programação de alto nível e uma linguagem de baixo nível?",
     "Uma linguagem de programação de alto nível é mais abstrata e próxima da linguagem humana, enquanto uma linguagem de baixo nível é mais próxima da linguagem da máquina."
 );
 
 criarCard(
-    "Programação",
+
     "O que é um loop em programação?",
     "Um loop é uma estrutura que permite que um conjunto de instruções seja executado repetidamente enquanto uma condição é verdadeira."
 );
 
 criarCard(
-    "Programação",
+
     "Quais são os três tipos principais de estruturas de controle em programação?",
     "Os três principais tipos de estruturas de controle são sequência, seleção (por exemplo, if-else) e repetição (por exemplo, loops)."
 );
 
 criarCard(
-    "Programação",
+
     "O que é uma variável em programação?",
     "Uma variável é um espaço na memória que armazena um valor ou uma referência a um valor."
 );
 
 criarCard(
-    "Programação",
+
     "Qual é a finalidade da declaração 'if' em programação?",
     "A declaração 'if' é usada para executar um bloco de código apenas se uma determinada condição for verdadeira."
 );
 
 criarCard(
-    "Programação",
+
     "O que é uma função em programação?",
     "Uma função é um bloco de código que realiza uma tarefa específica e pode ser chamada em diferentes partes do programa."
 );
 
 criarCard(
-    "Programação",
+
     "O que significa 'depurar' um programa?",
     "Depurar um programa significa identificar e corrigir erros ou bugs no código."
 );
 
 criarCard(
-    "Programação",
+
     "Qual é a importância dos comentários no código de programação?",
     "Comentários são usados para explicar o código, tornando-o mais compreensível para outros programadores e para o próprio desenvolvedor no futuro."
 );
 
 criarCard(
-    "Programação",
+
     "O que é uma linguagem de marcação? Dê um exemplo.",
     "Uma linguagem de marcação é uma linguagem usada para formatar e organizar conteúdo, como HTML para páginas web ou XML para dados estruturados."
 );
 
 criarCard(
-    "Programação",
+
     "O que é HTML e qual é o seu papel no desenvolvimento web?",
     "HTML é uma linguagem de marcação usada para estruturar o conteúdo de páginas web, definindo elementos como cabeçalhos, parágrafos, links e imagens."
 );
 
-// ... Continuar com as outras perguntas de programação
-
-// Matemática
 criarCard(
-    "Matemática",
+
     "Resolva a expressão: 3 + 7 * 2.",
     "A resposta é 17."
 );
 
 criarCard(
-    "Matemática",
+
     "O que é uma equação linear?",
     "Uma equação linear é uma equação que representa uma reta no gráfico."
 );
 
 criarCard(
-    "Matemática",
+
     "Resolva a equação para x: 2x - 5 = 11.",
     "A solução é x = 8."
 );
 
 criarCard(
-    "Matemática",
+
     "Qual é o teorema de Pitágoras?",
     "O teorema de Pitágoras afirma que, em um triângulo retângulo, o quadrado da hipotenusa é igual à soma dos quadrados dos catetos."
 );
 
 criarCard(
-    "Matemática",
+
     "O que são números primos? Dê um exemplo.",
     "Números primos são números maiores que 1 que têm apenas dois divisores positivos: 1 e eles mesmos. Exemplo: 7."
 );
 
 criarCard(
-    "Matemática",
+
     "Qual é a diferença entre média aritmética e média ponderada?",
     "A média aritmética é a soma de todos os valores dividida pelo número de valores. A média ponderada leva em consideração pesos diferentes para cada valor."
 );
 
 criarCard(
-    "Matemática",
+
     "Resolva o sistema de equações:\n2x + y = 10\nx - y = 4",
     "A solução é x = 7 e y = 3."
 );
 
 criarCard(
-    "Matemática",
+
     "O que é uma sequência aritmética?",
     "Uma sequência aritmética é uma sequência numérica em que a diferença entre dois termos consecutivos é sempre constante."
 );
 
 criarCard(
-    "Matemática",
+
     "Calcule a área de um triângulo com base 8 e altura 12.",
     "A área é igual a 48 unidades quadradas."
 );
 
 criarCard(
-    "Matemática",
+
     "O que é um polígono regular?",
     "Um polígono regular é um polígono cujos lados e ângulos internos são todos congruentes (iguais)."
 );
