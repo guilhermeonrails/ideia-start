f2d8d7d3da1e013453f3a5c75cae38d4f86b4acb
guilhermelima <guilhermelimaruby@gmail.com>
2023-08-21 14:32:16 -0300

    ideia 3

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 function criarCard(titulo, pergunta, resposta) {
   const cardContainer = document.getElementById("cardContainer");
 
-  const card = document.createElement("div");
+  const card = document.createElement("card");
   card.className = "card";
   card.innerHTML = `
     <h2>${titulo}</h2>
     <p>${pergunta}</p>
     <p class="answer">${resposta}</p>
   `;
+
+  card.setAttribute("role", "article");
+  card.setAttribute("aria-label", titulo);
 
   card.addEventListener("click", () => {
     const answer = card.querySelector(".answer");
