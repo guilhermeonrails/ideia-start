c7c8be0dfea5515d1658fc0cc52e459c9579fcae
guilhermelima <guilhermelimaruby@gmail.com>
2023-08-22 14:45:11 -0300

    animation

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 function criarCard(pergunta, resposta) {
-  const cardContainer = document.getElementById("cardContainer");
+  const cardContainer = document.getElementById("container");
 
-  const card = document.createElement("card");
+  const card = document.createElement("div");
   card.className = "card";
+
   card.innerHTML = `
-    <p>${pergunta}</p>
-    <p class="resposta">${resposta}</p>
+    <div class="card__content">
+      <div class="card__content__front">
+        <p>${pergunta}</p>
+      </div>
+      <div class="card__content__back">
+        <p>${resposta}</p>
+      </div>
+    </div>
   `;
 
-  card.addEventListener("click", () => {
-    const answer = card.querySelector(".resposta");
-    const isAnswerVisible = answer.style.display === "block";
+  let respostaEstaVisivel = false;
 
-    answer.style.display = isAnswerVisible ? "none" : "block";
+  card.addEventListener('click', () => {
+    respostaEstaVisivel = !respostaEstaVisivel;
+    card.classList.toggle('active', respostaEstaVisivel);
   });
 
   cardContainer.appendChild(card);
