01d819f7b8bc31c475071f3efcc06d8bf106e107
guilhermelima <guilhermelimaruby@gmail.com>
2023-08-23 15:25:18 -0300

    fix size

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,59 @@
 criarCard(
-    "Programação",
-    "O que é um algoritmo?",
-    "Um algoritmo é uma sequência de instruções que descrevem um processo para resolver um problema ou executar uma tarefa."
+    "Lógica",
+    "O que é uma string?",
+    "É uma sequência de caracteres alfanuméricos (letras, números e/ou símbolos)."
 );
 
 criarCard(
-    "Programação",
-    "Qual é a diferença entre uma linguagem de programação de alto nível e uma linguagem de baixo nível?",
-    "Uma linguagem de programação de alto nível é mais abstrata e próxima da linguagem humana, enquanto uma linguagem de baixo nível é mais próxima da linguagem da máquina."
-);
-
-// ... (outros cartões de programação)
-
-criarCard(
-    "Matemática",
-    "O que é uma equação linear?",
-    "Uma equação linear é uma equação que representa uma reta no gráfico."
+    "Variáveis",
+    "O que é uma variável?",
+    "Uma variável é um nome associado a um valor ou referência a um valor na memória."
 );
 
 criarCard(
-    "Matemática",
-    "Resolva a equação para x: 2x - 5 = 11.",
-    "A solução é x = 8."
-);
-
-// ... (outros cartões de matemática)
-
-criarCard(
-    "JavaScript",
-    "O que é JavaScript?",
-    "JavaScript é uma linguagem de programação amplamente usada para desenvolvimento web, permitindo interatividade e dinamismo em páginas."
+    "Condições",
+    "O que é uma declaração 'if'?",
+    "A declaração 'if' é usada para executar um bloco de código se uma condição for verdadeira."
 );
 
 criarCard(
-    "JavaScript",
-    "Como declarar uma variável em JavaScript?",
-    "Uma variável em JavaScript pode ser declarada usando 'var', 'let' ou 'const', seguido pelo nome da variável."
-);
-
-
-criarCard(
-    "Geografia",
-    "O que é latitude?",
-    "A latitude é a distância medida em graus de um ponto da Terra em relação à linha do Equador, indicando a sua posição norte ou sul."
+    "Loops",
+    "O que é um loop 'for'?",
+    "Um loop 'for' é uma estrutura de repetição que executa um bloco de código várias vezes, baseado em uma contagem."
 );
 
 criarCard(
-    "Geografia",
-    "Qual é a diferença entre tempo e clima?",
-    "Tempo refere-se às condições atmosféricas em um curto período, enquanto clima se refere ao padrão de condições atmosféricas em uma região ao longo do tempo."
+    "Funções",
+    "O que é uma função?",
+    "Uma função é um bloco de código reutilizável que realiza uma tarefa específica quando chamado."
 );
 
+criarCard(
+    "Arrays",
+    "O que é um array?",
+    "Um array é uma estrutura de dados que armazena uma coleção ordenada de elementos do mesmo tipo."
+);
+
+criarCard(
+    "Objetos",
+    "O que é um objeto em programação?",
+    "Um objeto é uma estrutura que contém propriedades e métodos relacionados a um conceito ou entidade."
+);
+
+criarCard(
+    "Métodos",
+    "O que são métodos?",
+    "Métodos são funções associadas a objetos que realizam ações específicas sobre eles."
+);
+
+criarCard(
+    "Classes",
+    "O que é uma classe?",
+    "Uma classe é um modelo ou blueprint para criar objetos com propriedades e comportamentos específicos."
+);
+
+criarCard(
+    "Escopo",
+    "O que é escopo em programação?",
+    "Escopo define onde uma variável ou função é visível e acessível dentro do código."
+);
